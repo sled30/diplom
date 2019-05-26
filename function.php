5762cf8bf6ffa132449c6bc69c913610fdaf71dd
sled <max-30@mail.ru>
2019-05-26 23:45:48 +0400

    паррсинг главной страницы получение урл тема направление

--- function.php
+++ function.php
@@ -1,9 +1,34 @@
 <?php
 function parser_rsi($data_rsi){
     // code...
-    $date = find_date_grant_rsi($data_rsi);
+  /*  $date = find_date_grant_rsi($data_rsi);
     $url = find_url_grant_rsi($data_rsi);
-    var_dump($url);
+    $teg = find_teg_grant_rsi($data_rsi);
+    var_dump($teg);*/
+  find($data_rsi);
+}
+function find($data_rsi){
+  // code...
+  $match = '/<span>(\d{2}.\d{2})<\/span>
+						<span>(\d{4})<\/span>
+					<\/time>
+								.{0,}
+					.{0,}
+						<img src="\/upload\/iblock\/\S{3}\/.{3,}.jpg" alt="(.{3,})">
+						.{7,}
+							.{0,}>(.{4,})<\/a><\/div>
+						<\/div>
+					<\/div>
+					.{8,}
+						<a href="(\/grants\/grant_news\/\d{1,}\/\d{3,}.php)" ><h4 class="text-title">(.{3,})\/h4/';
+    preg_match_all($match, $data_rsi, $list);
+    var_dump($list);
+}
+function find_teg_grant_rsi($data_rsi){
+  // code...
+  $match = '/"info-branch"><a href="\/grants\/grant_news\/\d{3}\/">(\D{9,})<\/a><\/div>/';
+  preg_match_all($match, $data_rsi, $list);
+  var_dump($list);
 
 }
 function find_url_grant_rsi($date_rsi){
@@ -12,8 +37,6 @@ function find_url_grant_rsi($date_rsi){
 						<a href="(\/grants\/grant_news\/\d{1,4}\/\d{1,7}.php)" ><h4/';
   preg_match_all($match, $date_rsi, $list);
   return $list[1];
-
-
 }
 function find_date_grant_rsi($data_rsi){
   // code...
